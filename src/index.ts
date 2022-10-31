6976a88efad725d6834ae7110b3c9fda5f0a7436
Totto16 <tobiausgais@gmail.com>
2022-10-31 20:39:41 +0100

    - added better visuals, it can display the opcode information on hover

--- src/index.ts
+++ src/index.ts
@@ -32,11 +32,25 @@ function start() {
     }
 
     inputElement.onchange = onChooseFile
+
+    const romList: HTMLUListElement | null =
+        document.querySelector("ul#rom-list")
+    if (romList === null) {
+        throw new Error("No rom list element was found!")
+    }
+
+    const allRoms = Array.from(romList.children) as HTMLElement[]
+
+    allRoms.forEach((rom: HTMLElement) => {
+        rom.onclick = () => {
+            const name = rom.innerText
+            fetchFile(name).then((arr) => loadProgramm(arr, name))
+        }
+    })
 }
 
 function generateUI(array: Uint8ClampedArray, contentElement: HTMLElement) {
-    
-    const hoverOverElement = (element: HTMLElement, opCode : OpCode)=>{
+    const hoverOverElement = (element: HTMLElement, opCode: OpCode) => {
         console.log(opCode)
     }
 
@@ -55,31 +69,110 @@ function generateUI(array: Uint8ClampedArray, contentElement: HTMLElement) {
         .fill(undefined)
         .map((_, i) => {
             return Instruction.toHexString(
-                BigInt(ENTRY_POINT + i * Instruction.SIZE), false
+                BigInt(ENTRY_POINT + i * Instruction.SIZE),
+                false
             )
         })
 
-        
-    contentElement.innerHTML = `<div class="inst-container"><div class="inst-addresses">${addresses.map(
-        (addr) => {
-            return `<div class="address-lines">${addr}</div>`
+    const root = document.createElement("div")
+    root.className = "inst-container"
+
+    const addressDiv = document.createElement("div")
+    addressDiv.className = "inst-addresses"
+
+    addresses.forEach((addr) => {
+        const singleAddress = document.createElement("div")
+        singleAddress.className = "address-lines"
+        singleAddress.innerHTML = addr
+
+        addressDiv.appendChild(singleAddress)
+    })
+
+    root.appendChild(addressDiv)
+
+    const codeDiv = document.createElement("div")
+    codeDiv.className = "inst-code"
+
+    readableByteCode.forEach(([opCode, bytes]) => {
+        const singleInst = document.createElement("div")
+        singleInst.className = "outer-inst"
+
+        const info = opCode.getDescription()
+
+        const instBytes = Instruction.toBEBytes(opCode.asInstruction())
+        const code: string[] = []
+
+        let i = 0
+        for (const [name, value, byteLength] of info) {
+            const bytes = instBytes.slice(i, i + byteLength)
+
+            code.push(
+                `<div class="inner-inst" attr-name="${name}" attr-value="${value.toString()}">${bytes
+                    .map((byte) => Byte.toHexString([byte], true, true, false))
+                    .join(" ")}</div>`
+            )
+            i += byteLength
         }
-    ).join("")}</div><div class="inst-code">${readableByteCode
-        .map(([opCode, bytes]) => {
-            const code = bytes.map((byte) =>
-                Byte.toHexString([byte], true, true, false)
+
+        if (i != Instruction.SIZE) {
+            code.push(
+                `<div class="inner-inst">${instBytes
+                    .slice(i)
+                    .map((byte) => Byte.toHexString([byte], true, true, false))
+                    .join(" ")}</div>`
             )
+        }
 
-            return `<div class="outer-inst" onclick="console.log('${
-                opCode.name
-            }')">${code.join(" ")}</div>`
-        })
-        .join("")}</div>`
+        const hoverChild = document.createElement("div")
+        hoverChild.className = "hover-inst"
 
-    console.log("generated UI successfully!")
+        const hoverName = document.createElement("div")
+        hoverName.className = "inst-name"
+        hoverName.innerHTML = opCode.name
+
+        hoverChild.appendChild(hoverName)
+
+        for (const [key, value] of Object.entries(opCode.parsedInstruction)) {
+            console.log(key, ["cycles", "increment", "opCode"].includes(key))
+            if (["cycles", "increment", "opCode"].includes(key)) {
+                continue
+            }
+            const singleEl = document.createElement("div")
+            singleEl.className = "inst-attr"
+            singleEl.setAttribute(key, value)
+            singleEl.innerHTML = `<span>${key}: ${value}</span>`
+
+            hoverChild.appendChild(singleEl)
+        }
+
+        singleInst.onmouseenter = () => {
+            singleInst.appendChild(hoverChild)
+        }
+
+        singleInst.onmouseleave = () => {
+            singleInst.removeChild(hoverChild)
+        }
+
+        singleInst.innerHTML = code.join(" ")
+
+        codeDiv.appendChild(singleInst)
+    })
+
+    root.appendChild(codeDiv)
+
+    contentElement.appendChild(root)
 }
 
-function onChooseFile(_event: Event) {
+async function fetchFile(url: string): Promise<ArrayBuffer> {
+    const resp = await fetch(url)
+    if (!resp.ok || resp.status < 200 || resp.status >= 400) {
+        throw new Error(`Fetching ${url} went wrong: ${resp}`)
+    }
+
+    return (await resp.blob()).arrayBuffer()
+}
+
+async function onChooseFile(_event: Event) {
     if (typeof window.FileReader !== "function") {
         throw "The file API isn't supported on this browser."
     }
@@ -105,56 +198,59 @@ function onChooseFile(_event: Event) {
         throw new Error("not a backseat file")
     }
 
-    file.arrayBuffer()
-        .then((content) => {
-            const contentElement: HTMLParagraphElement | null =
-                document.querySelector("p#contents")
-            if (contentElement === null) {
-                throw new Error("No paragraph element was found!")
-            }
+    const content = await file.arrayBuffer()
 
-            const array = new Uint8ClampedArray(content)
+    loadProgramm(content, file.name)
+}
 
-            if (array.byteLength % Instruction.SIZE) {
-                throw new Error(
-                    `The file doesn't seem to have a valid length to fit instructions in: ${array.byteLength} % ${Instruction.SIZE} != 0`
-                )
-            }
+async function loadProgramm(content: ArrayBuffer, name: string) {
+    const contentElement: HTMLDivElement | null =
+        document.querySelector("div#contents")
+    if (contentElement === null) {
+        throw new Error("No paragraph element was found!")
+    }
 
-            generateUI(array, contentElement)
+    const array = new Uint8ClampedArray(content)
 
-            // TODO make buttons and checkboxes to manipulate the args, run , emit, json and the option like exit on halt
+    if (array.byteLength % Instruction.SIZE) {
+        throw new Error(
+            `The file doesn't seem to have a valid length to fit instructions in: ${array.byteLength} % ${Instruction.SIZE} != 0`
+        )
+    }
 
-            const enableStepping = document.querySelector<HTMLInputElement>(
-                'input#enable-stepping[type="checkbox"]'
-            )
+    generateUI(array, contentElement)
 
-            const manuallyStep =
-                enableStepping && enableStepping.checked
-                    ? document.querySelector<HTMLButtonElement>(
-                          "button#nextButton"
-                      ) ?? undefined
-                    : undefined
+    // TODO make buttons and checkboxes to manipulate the args, run , emit, json and the option like exit on halt
 
-            runProgramm(
-                getDrawHandle(true),
-                {
-                    file,
-                    content: array,
-                },
-                {
-                    action: "Run",
-                    arguments: { path: file.name, exitOnHalt: true },
-                },
-                manuallyStep
-            )
-        })
-        .then(() => console.log("finished running the program"))
+    const enableStepping = document.querySelector<HTMLInputElement>(
+        'input#enable-stepping[type="checkbox"]'
+    )
+
+    const manuallyStep =
+        enableStepping && enableStepping.checked
+            ? document.querySelector<HTMLButtonElement>("button#nextButton") ??
+              undefined
+            : undefined
+
+    await runProgramm(
+        getDrawHandle(true),
+        {
+            name,
+            content: array,
+        },
+        {
+            action: "Run",
+            arguments: { path: name, exitOnHalt: true },
+        },
+        manuallyStep
+    )
+
+    console.log("finished running the program")
 }
 
 start()
 
 export interface PseudoFile {
-    file: File
+    name: string
     content: Uint8ClampedArray
 }
